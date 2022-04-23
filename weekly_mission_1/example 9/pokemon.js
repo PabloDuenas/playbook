be0f14ff8838ccba0c86cb2d85c90d447bcd583e
PabloDuenas <99110015+PabloDuenas@users.noreply.github.com>
2022-04-23 00:29:43 -0500

    Update pokemon.js

--- weekly_mission_1/example 9/pokemon.js
+++ weekly_mission_1/example 9/pokemon.js
@@ -1,7 +1,16 @@
 class pokemon {
 constructor(name) {
-function sayMesagge{};
+function sayHello{};
 
 
 
+export default class pokemon {
+  constructor (name) {
+    this.name = name
+  }
+
+  log (message) {
+    console.log(`[${this.name}] ${message}`)
+  }
+}
 
