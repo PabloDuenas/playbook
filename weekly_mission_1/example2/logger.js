362542d7a66e906ba9bdcf762d1bca78b7e61c5a
PabloDuenas <99110015+PabloDuenas@users.noreply.github.com>
2022-04-22 23:46:57 -0500

    Update logger.js
    Ejercicio 2.1

--- weekly_mission_1/example2/logger.js
+++ weekly_mission_1/example2/logger.js
@@ -1,1 +1,15 @@
+exports.info = (message) => {
+  console.log(`info: ${message}`)
+}
+
+// Esta es una función que se guardara en este módulo como verbose
+exports.verbose = (message) => {
+  console.log(`verbose: ${message}`)
+}
+
+/*
+  const logger = require('./logger')
+  logger.info('This is an informational message')
+  logger.verbose('This is a verbose message')
+ * */
 
