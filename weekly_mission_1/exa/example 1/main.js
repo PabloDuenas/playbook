9aa88bd794ded283a862a02fcb08b648c70afc41
PabloDuenas <99110015+PabloDuenas@users.noreply.github.com>
2022-04-22 23:43:50 -0500

    Update main.js
    Primer ejemplo

--- weekly_mission_1/exa/example 1/main.js
+++ weekly_mission_1/exa/example 1/main.js
@@ -1,1 +1,32 @@
+let myCar = new Object()
+myCar.make = 'Ford';
+myCar.model = 'Mustang';
+myCar.year = '1969;
+console.Log(myCar)
+
+
+cost myModule = (() => {
+  cost privateFoo = "Soy un valor privado, solo me usan dentro de este objeto"
+  cost privateBar = [1, 2, 3, 4,]
+  cost baz = "Soy un valor que va a ser expuesto"
+  
+  cost exported ={
+    publicFoo: "Valor publico, puden verme desde donde me llamen",
+    publicBar: "otro valor publico",
+          publicBaz: baz
+  }
+  
+  return exported
+})()
+
+console.log(myModule)
+  
+              
+                 
+                 
+                 
+                 
+                 
+                 
+
 
